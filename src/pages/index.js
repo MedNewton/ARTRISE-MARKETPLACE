4047aa724d0894ec81f4a6b9f0cf367647f21e75
Syed Ubaid <plkubaid@gmail.com>
2023-11-23 12:04:42 -0500

    Offer System Created.

--- src/pages/index.js
+++ src/pages/index.js
@@ -65,6 +65,8 @@ import Collection from "../components/layouts/Collections/Collection";
 import Learn from "./Learn";
 import LearnResourceDetail from "./LearnResourceDetail";
 import OrderTracking from "./OrderTracking/OrderTracking";
+import OfferList from "./OfferSystem/OfferList";
+import OfferSent from "./OfferSystem/OfferSent";
 
 const routes = [
   { path: '/', component: <Home01 />},
@@ -133,7 +135,10 @@ const routes = [
   { path: '/collection', component:<Collection/>},
   { path: '/learn', component:<Learn/>},
   { path: '/learn/*', component: <LearnResourceDetail/>},
-  { path: '/order/tracking', component: <OrderTracking/>}
+  { path: '/order/tracking', component: <OrderTracking/>},
+  { path: '/page/offer/received', component: <OfferList/>},
+  { path: '/page/offer/sent', component: <OfferSent/>},
+  
   
 
 ]
