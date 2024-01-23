3dd9054e9ba980b02888b227bf091ecd042e13c3
fazal-a <thefazalabbas@gmail.com>
2024-01-23 06:57:19 +0500

    improved createChoices page

--- src/pages/createChoices.jsx
+++ src/pages/createChoices.jsx
@@ -6,59 +6,54 @@ import CreateCollectionModal from '../components/layouts/createCollectionModal';
 
 function CreateChoices() {
   const [createModalShow, setCreateModalShow] = useState(false);
-
   return (
-    <div>
-      <div className="tf-create-item tf-section">
-        <div className="themesflat-container">
-          <div className="row top-bottom-padding " style={{ paddingLeft: '0.5%' }}>
-            <div className="col-md-12" style={{ marginBottom: '5%' }}>
-              <h2
-                className="tf-title style4 mg-bt-38 ourArtists"
-                style={{ marginTop: '5%', fontWeight: '700' }}
-              >
-                Create
-              </h2>
+    <div className="createChoicesPageWrapper">
+      <div>
+        <h2
+          className="tf-title style4 mg-bt-38 ourArtists"
+          style={{
+            marginTop: '5%',
+            fontWeight: '700',
+          }}
+        >
+          Create
+        </h2>
+        <div className="createChoiceBoxesWrapper">
+          <button
+            type="button"
+            className="createChoiceBox"
+            onClick={() => {
+              setCreateModalShow(true);
+            }}
+          >
+            <div>
+              <h5>Create a collection</h5>
+              <p>
+                Showcase your artworks ina visually stunning
+                collection. chose a theme, add descriptions and make it uniquely yours. Your
+                collection will be available for purchase on our marketplace.
+              </p>
             </div>
-            <div className="createChoiceBoxesWrapper" style={{ paddingLeft: '1%' }}>
-              <button
-                type="button"
-                className="row createChoiceBox"
-                style={{ paddingRight: '5%' }}
-                onClick={() => {
-                  setCreateModalShow(true);
-                }}
-              >
-                <div className="col-md-6 createChoiceTextBox">
-                  <h5 className="createChoiceTitle">Create a collection</h5>
-                  <h5 className="createChoiceLabel">
-                    Showcase your artworks ina visually stunning
-                    collection. chose a theme, add descriptions and make it uniquely yours. Your
-                    collection will be available for purchase on our marketplace.
-                  </h5>
-                </div>
-                <div className="col-md-6 createChoiceImageBox">
-                  <div className="createChoiceImage" />
-                </div>
-              </button>
-              <Link
-                to="/create-item"
-                className="row createChoiceBox"
-              >
-                <div className="col-md-6 createChoiceTextBox">
-                  <h5 className="createChoiceTitle">Create an artwork</h5>
-                  <h5 className="createChoiceLabel">
-                    Integrate your original artowrk and turn it into a
-                    hybrid NFT. Simply upload a photo of your artwork, fill out the artwork details
-                    and properties, and we&apos;ll take care of the rest.
-                  </h5>
-                </div>
-                <div className="col-md-6 createChoiceImageBox">
-                  <div className="createChoiceImage" />
-                </div>
-              </Link>
+            <div className="col-md-6 createChoiceImageBox">
+              <div className="createChoiceImage" />
             </div>
-          </div>
+          </button>
+          <Link
+            to="/create-item"
+            className="createChoiceBox"
+          >
+            <div>
+              <h5>Create an artwork</h5>
+              <p>
+                Integrate your original artowrk and turn it into a
+                hybrid NFT. Simply upload a photo of your artwork, fill out the artwork details
+                and properties, and we&apos;ll take care of the rest.
+              </p>
+            </div>
+            <div className="col-md-6 createChoiceImageBox">
+              <div className="createChoiceImage" />
+            </div>
+          </Link>
         </div>
       </div>
       <CreateCollectionModal
