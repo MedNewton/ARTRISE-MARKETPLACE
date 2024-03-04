aadd59c8bb4326649f40b77322526942061b9623
fazal-a <thefazalabbas@gmail.com>
2024-03-04 23:44:50 +0500

    created ItemGrid.style to render the four artworks

---
+++ src/components/layouts/craftsmen/craftsmenStyles/ImageGrid.style.js
@@ -0,0 +1,16 @@
+/* eslint-disable */
+import styled from 'styled-components';
+
+export const ImageGridContainer = styled.div`
+    width: 100%;
+    height: 100%;
+    gap: 10px;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+`;
+
+export const ImageWrapper = styled.div`
+    max-width: 50%;
+    width: 48%;
+`;
