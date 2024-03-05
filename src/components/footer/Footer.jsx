1fd55b4d15b884d749ce4d0c64e907a1ac9846e6
fazal-a <thefazalabbas@gmail.com>
2024-03-06 01:48:30 +0500

    updated app version in footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -225,7 +225,7 @@ function Footer() {
           <div className="footer-copyrights">
 
             <div><p className="rights">All rights reserved ArtRise Labs LLC</p></div>
-            <div><p className="rights">Version: January 23, 2024</p></div>
+            <div><p className="rights">Version: March 6, 2024</p></div>
             <div>
               {' '}
               <p className="rights privacy">
