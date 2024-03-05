a91763b28b7014e07891f5ef3e5677543f00798f
fazal-a <thefazalabbas@gmail.com>
2024-03-05 23:33:49 +0500

    improved logic in MediaViewer file

--- src/components/layouts/mediaViewer/MediaViewer.jsx
+++ src/components/layouts/mediaViewer/MediaViewer.jsx
@@ -2,57 +2,66 @@ import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import placeHolderMainImage from '../../../assets/images/box-item/collection-item-bottom-4.jpg';
 
-function MediaViewer(props) {
-  const { mediaUrl } = props;
-  const [isVideo, setIsVideo] = useState(false);
-  const [isImage, setIsImage] = useState(false);
+function MediaViewer({ mediaUrl }) {
+  const [mediaType, setMediaType] = useState(''); // Possible values: 'image', 'video', 'unknown'
 
   useEffect(() => {
-    fetch(mediaUrl)
-      .then((response) => {
+    const fetchMediaType = async () => {
+      if (!mediaUrl) {
+        setMediaType('unknown');
+        return;
+      }
+
+      try {
+        const response = await fetch(mediaUrl);
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
         const contentType = response.headers.get('Content-Type');
         if (contentType?.includes('video')) {
-          setIsVideo(true);
-          setIsImage(false);
+          setMediaType('video');
         } else if (contentType?.includes('image')) {
-          setIsVideo(false);
-          setIsImage(true);
+          setMediaType('image');
+        } else {
+          setMediaType('unknown');
         }
-      });
+      } catch (error) {
+        console.error('Error fetching media:', error);
+        setMediaType('unknown');
+      }
+    };
+
+    fetchMediaType();
   }, [mediaUrl]);
 
-  if (isVideo) {
-    return (
-      <video autoPlay muted loop>
-        <source src={mediaUrl} type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
-    );
+  switch (mediaType) {
+    case 'video':
+      return (
+        <video autoPlay muted loop>
+          <source src={mediaUrl} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      );
+    case 'image':
+      return (
+        <img
+          src={mediaUrl}
+          alt="Artwork"
+        />
+      );
+    case 'unknown':
+    default:
+      return (
+        <img
+          src={placeHolderMainImage}
+          alt="Placeholder"
+        />
+      );
   }
-
-  if (isImage) {
-    return (
-      <img
-        src={mediaUrl}
-        alt="Artwork data not available"
-      />
-    );
-  }
-
-  return (
-    <img
-      src={placeHolderMainImage}
-      alt="Artwork data not available"
-    />
-  );
 }
 
 MediaViewer.propTypes = {
-  mediaUrl: PropTypes.string,
-};
-
-MediaViewer.defaultProps = {
-  mediaUrl: '', // Provide a default value (e.g., an empty string) or null based on your use case.
+  mediaUrl: PropTypes.string.isRequired,
 };
 
 export default MediaViewer;
