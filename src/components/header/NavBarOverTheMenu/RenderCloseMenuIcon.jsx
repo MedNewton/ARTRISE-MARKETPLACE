0d3e1ce2cb8a49979038fabe6704291133499a67
fazal-a <thefazalabbas@gmail.com>
2024-03-19 01:22:04 +0500

    worked on CloseMenuIcon page

--- src/components/header/NavBarOverTheMenu/RenderCloseMenuIcon.jsx
+++ src/components/header/NavBarOverTheMenu/RenderCloseMenuIcon.jsx
@@ -1,24 +1,25 @@
 import React from 'react';
 import { AiOutlineClose } from 'react-icons/ai';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
+const ButtonWrapper = styled.div`
+    cursor: pointer
+  `;
 function RenderCloseMenuIcon(props) {
   const { handleMenuModalClose } = props;
 
   return (
-    <>
+    <ButtonWrapper
+      className="d-flex justify-content-center align-items-center"
+      onClick={
+            () => {
+              handleMenuModalClose();
+            }
+        }
+    >
       <AiOutlineClose id="close-menu-icon" style={{ width: '30px', height: '30px' }} />
-      <button
-        type="button"
-        className="d-flex justify-content-center align-items-center"
-        onClick={
-                 () => {
-                   handleMenuModalClose();
-                 }
-             }
-        aria-labelledby="close-menu-icon"
-      />
-    </>
+    </ButtonWrapper>
   );
 }
 
