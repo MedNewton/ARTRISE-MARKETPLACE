964d143a257837617742b9fffee0c42ad6148f10
fazal-a <thefazalabbas@gmail.com>
2024-03-19 01:21:25 +0500

    worked on HeaderSearchPage page

--- src/components/header/HeaderSearch/HeaderSearchForMobileView.jsx
+++ src/components/header/HeaderSearch/HeaderSearchForMobileView.jsx
@@ -1,21 +1,20 @@
-import { FaAngleLeft } from 'react-icons/fa';
 import React from 'react';
 import PropTypes from 'prop-types';
+import { IoIosArrowBack } from 'react-icons/io';
 import HeaderSearch from './HeaderSearch';
+import { SearchButton } from '../HeaderStyles/HeaderSearchForMobileView.style';
 
 function HeaderSearchForMobileView(props) {
   const { setShowSearchField } = props;
   return (
     <div className="header-search-mobile-version">
-      <div id="angle-left-icon" style={{ fontSize: 'large' }}><FaAngleLeft /></div>
-      <button
-        type="button"
-        style={{ width: '10%' }}
+      <SearchButton
         onClick={() => {
           setShowSearchField(false);
         }}
-        aria-labelledby="angle-left-icon"
-      />
+      >
+        <IoIosArrowBack style={{ fontSize: 'xx-large' }} />
+      </SearchButton>
       <div style={{ width: '90%' }}>
         <HeaderSearch />
       </div>
