cdf363f25118d691d155bb0d39e53bd31c8f6e84
fazal-a <thefazalabbas@gmail.com>
2024-05-13 23:53:59 +0500

    added display profile headings file

---
+++ src/components/constants/DisplayProfileTabsHeadings.js
@@ -0,0 +1,138 @@
+/*eslint-disable*/
+
+export const ArtistDesktopViewMainTabs= [
+    {
+        key: 0,
+        name: 'Artworks',
+    },
+    {
+        key: 1,
+        name: 'Collections',
+    },
+    {
+        key: 2,
+        name: 'Drops',
+    },
+    {
+        key: 3,
+        name: 'About',
+    },
+];
+
+export const ArtistDesktopViewMoreTabs=  [
+    {
+        key: 4,
+        name: 'Artworks',
+    },
+    {
+        key: 5,
+        name: 'Liked Items',
+    },
+    {
+        key: 6,
+        name: 'Offers Made',
+    },
+    {
+        key: 7,
+        name: 'Offers Received',
+    },
+];
+
+export const ArtistMobileViewMainTabs=  [
+    {
+        key: 0,
+        name: 'Artworks',
+    },
+    {
+        key: 1,
+        name: 'Collections',
+    },
+];
+
+export const ArtistMobileViewMoreTabs=  [
+    {
+        key: 2,
+        name: 'Drops',
+    },
+    {
+        key: 3,
+        name: 'About',
+    },
+    {
+        key: 4,
+        name: 'Artworks',
+    },
+    {
+        key: 5,
+        name: 'Liked Items',
+    },
+    {
+        key: 6,
+        name: 'Offers Made',
+    },
+    {
+        key: 7,
+        name: 'Offers Received',
+    },
+];
+
+
+export const MemberDesktopViewMainTabs= [
+    {
+        key: 0,
+        name: 'Owned',
+    },
+    {
+        key: 1,
+        name: 'Tokenized',
+    },
+    {
+        key: 2,
+        name: 'Liked Items',
+    },
+    {
+        key: 3,
+        name: 'Collections',
+    },
+];
+
+export const MemberDesktopViewMoreTabs=  [
+    {
+        key: 4,
+        name: 'Offers Made',
+    },
+    {
+        key: 5,
+        name: 'Offers Received',
+    },
+];
+
+export const MemberMobileViewMainTabs=  [
+    {
+        key: '23',
+        name: 'Owned',
+    },
+    {
+        key: '24',
+        name: 'Tokenized',
+    },
+];
+
+export const MemberMobileViewMoreTabs=  [
+    {
+        key: 'Liked Items',
+        name: 'Liked Items',
+    },
+    {
+        key: 'Collections',
+        name: 'Collections',
+    },
+    {
+        key: 'Offers Made',
+        name: 'Offers Made',
+    },
+    {
+        key: 'Offers Received',
+        name: 'Offers Received',
+    },
+];
