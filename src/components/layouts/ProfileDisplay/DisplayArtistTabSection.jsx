fcfd8e452452a1d4f03fd65f4dfd41676e07d059
fazal-a <thefazalabbas@gmail.com>
2024-05-18 23:26:31 +0500

    initial draft working on DisplayArtistTabSection

--- src/components/layouts/ProfileDisplay/DisplayArtistTabSection.jsx
+++ src/components/layouts/ProfileDisplay/DisplayArtistTabSection.jsx
@@ -1,7 +1,5 @@
+/*eslint-disable*/
 import React, { useEffect, useMemo, useState } from 'react';
-import {
-  Tab, TabList, TabPanel, Tabs,
-} from 'react-tabs';
 import Dropdown from 'react-bootstrap/Dropdown';
 import { useAccount } from 'wagmi';
 import PropTypes from 'prop-types';
@@ -16,14 +14,30 @@ import DisplayMyOwnedNfts from './DisplayMyOwnedNfts';
 import DisplayMemberOwnedNfts from './DisplayMemberOwnerNfts';
 import { userDataStoreShape } from '../../types/user-type';
 import { collectionStoreShape } from '../../types/collection-type';
+import {MoreTab, Tab, TabList, TabPanel, Tabs} from "../tab/Tabs";
+import {
+  ArtistDesktopViewMainTabs,
+  artistDesktopViewMainTabs, ArtistDesktopViewMoreTabs, ArtistMobileViewMainTabs, ArtistMobileViewMoreTabs,
+  MemberDesktopViewMainTabs, MemberDesktopViewMoreTabs, MemberMobileViewMainTabs, MemberMobileViewMoreTabs
+} from "../../constants/DisplayProfileTabsHeadings";
+import {useMediaQuery} from "react-responsive";
 
 function DisplayArtistTabSection(props) {
   const {
     artistData, lazyListed, collections, currentUserId,
   } = props;
+  const isDeviceMobile = useMediaQuery({ query: '(max-width: 1200px)' });
+
   const [userArtworks, setUserArtworks] = useState([]);
   const [userCollections, setUserCollections] = useState([]);
   const { address } = useAccount();
+
+  const [mainTabs, setMainTabs] = useState([]);
+  const [moreTabs, setMoreTabs] = useState([]);
+
+
+
+
 
   useEffect(() => {
     const tempArtworksList = GetArtWorks(artistData.userId, lazyListed);
@@ -32,233 +46,74 @@ function DisplayArtistTabSection(props) {
     setUserCollections(tempCollectionsList);
   }, [artistData, lazyListed, collections]);
 
-  function generateUniqueId() {
-    return uuidv4();
+const getTabs = ()=>{
+  setMainTabs(artistData?.socialMediaVerified ? ArtistDesktopViewMainTabs : MemberDesktopViewMainTabs)
+  setMoreTabs( artistData?.socialMediaVerified ? ArtistDesktopViewMoreTabs : MemberDesktopViewMoreTabs)
+  if(isDeviceMobile){
+    setMainTabs(artistData?.socialMediaVerified ? ArtistMobileViewMainTabs : MemberMobileViewMainTabs)
+    setMoreTabs( artistData?.socialMediaVerified ? ArtistMobileViewMoreTabs : MemberMobileViewMoreTabs)
   }
+}
+  useEffect(() => {
+    getTabs();
+  }, [isDeviceMobile, artistData]);
 
-  const artistViewMenuTabs = useMemo(() => [
-    {
-      class: 'active',
-      name: 'Artworks',
-    },
-    {
-      class: '',
-      name: 'Collections',
-    },
-    {
-      class: '',
-      name: 'Drops',
-    },
-    {
-      class: '',
-      name: 'About',
-    },
-  ], []);
+console.log("asdf  mainTabs::::",mainTabs);
+console.log("asdf  moreTabs::::",moreTabs)
 
-  const memberViewMenuTabs = useMemo(() => [
-    {
-      class: 'active',
-      name: 'Owned',
-    },
-    {
-      class: '',
-      name: 'Tokenized',
-    },
-    {
-      class: '',
-      name: 'Liked Items',
-    },
-    {
-      class: '',
-      name: 'Collections',
-    },
-  ], []);
 
-  return (
-    <Tabs>
-      <TabList>
-        {artistData?.socialMediaVerified
-          ? artistViewMenuTabs.map((item) => (
-            <Tab key={item?.userId ? item?.userId : generateUniqueId()}>
-              {item.name}
-            </Tab>
-          ))
-          : memberViewMenuTabs.map((item) => (
-            <Tab key={item?.userId ? item?.userId : generateUniqueId()}>
-              {item.name}
-            </Tab>
-          ))}
+    return (
+          <Tabs>
+              <TabList>
+                  {mainTabs.map((item) => (
+                      <Tab index={item.key}>
+                          {item.name}
+                      </Tab>
+                  ))}
+                  <MoreTab baseIndex={mainTabs.length}>
+                      {moreTabs.map((item) => (
+                          <Tab index={item.key}>
+                              {item.name}
+                          </Tab>
+                      ))}
+                  </MoreTab>
+              </TabList>
+              {/*{mainTabs.concat(moreTabs).map((item, index) => (*/}
+              {/*    <TabPanel index={item.key}>*/}
+              {/*      {item.k}*/}
+              {/*        <p>Content of {item.name} Tab</p>*/}
+              {/*    </TabPanel>*/}
 
-        <Tab key={4}>
-          <div className="tagLink">
-            <Dropdown>
-              <Dropdown.Toggle id="profileTabDropdown">
-                <SlOptions />
-              </Dropdown.Toggle>
-              <Dropdown.Menu>
-                {artistData?.socialMediaVerified
-                                        && (
-                                        <Dropdown.Item href="/">
-                                          <p className="tagLinkDropdownItemText">
-                                            Owned
-                                          </p>
-                                        </Dropdown.Item>
-                                        )}
-                {artistData?.socialMediaVerified
-                                        && (
-                                        <Dropdown.Item href="/">
-                                          <p className="tagLinkDropdownItemText">
-                                            Liked Items
-                                          </p>
-                                        </Dropdown.Item>
-                                        )}
-                <Dropdown.Item href="/">
-                  <p className="tagLinkDropdownItemText">
-                    Offers Made
-                  </p>
-                </Dropdown.Item>
-                <Dropdown.Item href="/">
-                  <p className="tagLinkDropdownItemText">
-                    Offers Received
-                  </p>
-                </Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown>
-          </div>
-        </Tab>
-      </TabList>
-      {artistData.socialMediaVerified
-        ? (
-          <div>
-            <TabPanel key={0} style={{ margin: '0% 2%', width: 'auto' }}>
-              {artistData.userId === currentUserId
-                ? <DisplayLazyOwnedNfts /> : <DisplayArtworks data={userArtworks} />}
-            </TabPanel>
-            <TabPanel key={1} style={{ margin: '25px 2%' }}>
-              <DisplayCollections data={userCollections} />
-            </TabPanel>
-            <TabPanel key={2} style={{ margin: '25px 2%' }}>
-              <div />
-            </TabPanel>
-            <TabPanel key={3} style={{ margin: '25px 2%' }}>
-              <h5 className="bioTabText">{artistData?.bio}</h5>
-            </TabPanel>
-          </div>
-        )
-        : (
-          <>
-            <TabPanel key={0} style={{ margin: '25px 2%', width: 'auto' }}>
-              {artistData?.userId === currentUserId
-                ? <DisplayMyOwnedNfts address={address} />
-                : <DisplayMemberOwnedNfts address={artistData?.userId} artistData={artistData} />}
-            </TabPanel>
-            <TabPanel key={1} style={{ margin: '25px 2%' }}>
-              <div
-                style={{
-                  display: 'flex', flexDirection: 'column', alignItems: 'center', width: '100%',
-                }}
-              >
-                <div>
-                  <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
-                    <img
-                      style={{ width: '50%' }}
-                      src="https://cdn.dribbble.com/users/1693462/screenshots/3504905/media/e76b879fc2bb9ec2a1f92da0732eb608.gif"
-                      alt=""
-                    />
-                  </div>
-                  <div className="row sorry">
-                    <div className="col-12">
-                      <h2 className="tf-title-heading ct style-2 fs-30 mg-bt-10">
-                        Sorry, We Couldn’t Find Any Artworks that you have tokenized.
-                      </h2>
-                      <h5 className="sub-title help-center mg-bt-32 ">
-                        Maybe your wallet is not connected, or you don&apos;t have any owned NFTs.
-                        The content of this page will updated as soon as you
-                        purchase one or more
-                        {' '}
-                        <br />
-                        {' '}
-                        of our unique and amazing
-                        artworks.
-                      </h5>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </TabPanel>
-            <TabPanel key={2} style={{ margin: '25px 2%' }}>
-              <div
-                style={{
-                  display: 'flex', flexDirection: 'column', alignItems: 'center', width: '100%',
-                }}
-              >
+              {/*))}*/}
 
-                <div>
-                  <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
-                    <img
-                      style={{ width: '50%' }}
-                      src="https://cdn.dribbble.com/users/1693462/screenshots/3504905/media/e76b879fc2bb9ec2a1f92da0732eb608.gif"
-                      alt=""
-                    />
-                  </div>
-                  <div className="row sorry">
-                    <div className="col-12">
-                      <h2 className="tf-title-heading ct style-2 fs-30 mg-bt-10">
-                        Sorry, We Couldn’t Find Any Artworks that you liked.
-                      </h2>
-                      <h5 className="sub-title help-center mg-bt-32 ">
-                        Maybe your wallet is not connected, or you don&apos;t have any owned NFTs.
-                        The content of this page will updated as soon as you
-                        purchase one or more
-                        {' '}
-                        <br />
-                        {' '}
-                        of our unique and amazing
-                        artworks.
-                      </h5>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </TabPanel>
-            <TabPanel key={3} style={{ margin: '25px 2%' }}>
-              <div
-                style={{
-                  display: 'flex', flexDirection: 'column', alignItems: 'center', width: '100%',
-                }}
-              >
-
-                <div>
-                  <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
-                    <img
-                      style={{ width: '50%' }}
-                      src="https://cdn.dribbble.com/users/1693462/screenshots/3504905/media/e76b879fc2bb9ec2a1f92da0732eb608.gif"
-                      alt=""
-                    />
-                  </div>
-                  <div className="row sorry">
-                    <div className="col-12">
-                      <h2 className="tf-title-heading ct style-2 fs-30 mg-bt-10">
-                        Sorry, We Couldn’t Find Any Artworks that you own.
-                      </h2>
-                      <h5 className="sub-title help-center mg-bt-32 ">
-                        Maybe your wallet is not connected, or you don&apos;t have any
-                        owned NFTs. The content of this page will updated as soon
-                        as you purchase one or more
-                        {' '}
-                        <br />
-                        {' '}
-                        of our unique and
-                        amazing artworks.
-                      </h5>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </TabPanel>
-          </>
-        )}
-    </Tabs>
+            {artistData.socialMediaVerified ? (
+                <>
+                  <TabPanel index={0}>
+                    {artistData.userId === currentUserId
+                        ? <DisplayLazyOwnedNfts />
+                        : <DisplayArtworks data={userArtworks} />}
+                  </TabPanel>
+                  <TabPanel index={1}>
+                    <DisplayCollections data={userCollections} />
+                  </TabPanel>
+                  <TabPanel index={2} style={{ margin: '25px 2%' }}>
+                    <div /> {/* Placeholder for any specific content if needed */}
+                  </TabPanel>
+                  <TabPanel index={3} style={{ margin: '25px 2%' }}>
+                    <h5 className="bioTabText">{artistData?.bio}</h5>
+                  </TabPanel>
+                </>
+            ) : (
+                <>
+                  <TabPanel index={0} style={{ margin: '25px 2%', width: 'auto' }}>
+                    {artistData.userId === currentUserId
+                        ? <DisplayMyOwnedNfts address={address} />
+                        : <DisplayMemberOwnedNfts address={artistData.userId} artistData={artistData} />}
+                  </TabPanel>
+                  {/* Assume other TabPanels for other tabs are defined here similarly */}
+                </>
+            )}
+          </Tabs>
   );
 }
 
