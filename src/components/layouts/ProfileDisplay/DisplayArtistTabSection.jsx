92644f8af863450c0c27c291229f66287370abee
fazal-a <thefazalabbas@gmail.com>
2024-05-19 22:53:41 +0500

    completed artist page tabs work

--- src/components/layouts/ProfileDisplay/DisplayArtistTabSection.jsx
+++ src/components/layouts/ProfileDisplay/DisplayArtistTabSection.jsx
@@ -1,10 +1,8 @@
-/*eslint-disable*/
-import React, { useEffect, useMemo, useState } from 'react';
-import Dropdown from 'react-bootstrap/Dropdown';
+import React, { useEffect, useState } from 'react';
 import { useAccount } from 'wagmi';
 import PropTypes from 'prop-types';
-import { v4 as uuidv4 } from 'uuid';
-import { SlOptions } from 'react-icons/sl';
+import { useMediaQuery } from 'react-responsive';
+import { useSelector } from 'react-redux';
 import DisplayArtworks from './DisplayArtworks';
 import DisplayCollections from './DisplayCollections';
 import { GetArtWorks, GetCollections }
@@ -14,19 +12,21 @@ import DisplayMyOwnedNfts from './DisplayMyOwnedNfts';
 import DisplayMemberOwnedNfts from './DisplayMemberOwnerNfts';
 import { userDataStoreShape } from '../../types/user-type';
 import { collectionStoreShape } from '../../types/collection-type';
-import {MoreTab, Tab, TabList, TabPanel, Tabs} from "../tab/Tabs";
 import {
-  ArtistDesktopViewMainTabs,
-  artistDesktopViewMainTabs, ArtistDesktopViewMoreTabs, ArtistMobileViewMainTabs, ArtistMobileViewMoreTabs,
-  MemberDesktopViewMainTabs, MemberDesktopViewMoreTabs, MemberMobileViewMainTabs, MemberMobileViewMoreTabs
-} from "../../constants/DisplayProfileTabsHeadings";
-import {useMediaQuery} from "react-responsive";
+  MoreTab, Tab, TabList, TabPanel, Tabs,
+} from '../tab/Tabs';
+import {
+  ArtistDesktopViewMainTabs, ArtistDesktopViewMoreTabs, ArtistMobileViewMainTabs, ArtistMobileViewMoreTabs,
+  MemberDesktopViewMainTabs, MemberDesktopViewMoreTabs, MemberMobileViewMainTabs, MemberMobileViewMoreTabs,
+} from '../../constants/DisplayProfileTabsHeadings';
+import DisplayDefaultTab from './DisplayDefaultTab';
 
 function DisplayArtistTabSection(props) {
   const {
     artistData, lazyListed, collections, currentUserId,
   } = props;
   const isDeviceMobile = useMediaQuery({ query: '(max-width: 1200px)' });
+  const theme = useSelector((state) => state.themeReducer.theme);
 
   const [userArtworks, setUserArtworks] = useState([]);
   const [userCollections, setUserCollections] = useState([]);
@@ -35,10 +35,6 @@ function DisplayArtistTabSection(props) {
   const [mainTabs, setMainTabs] = useState([]);
   const [moreTabs, setMoreTabs] = useState([]);
 
-
-
-
-
   useEffect(() => {
     const tempArtworksList = GetArtWorks(artistData.userId, lazyListed);
     setUserArtworks(tempArtworksList);
@@ -46,74 +42,86 @@ function DisplayArtistTabSection(props) {
     setUserCollections(tempCollectionsList);
   }, [artistData, lazyListed, collections]);
 
-const getTabs = ()=>{
-  setMainTabs(artistData?.socialMediaVerified ? ArtistDesktopViewMainTabs : MemberDesktopViewMainTabs)
-  setMoreTabs( artistData?.socialMediaVerified ? ArtistDesktopViewMoreTabs : MemberDesktopViewMoreTabs)
-  if(isDeviceMobile){
-    setMainTabs(artistData?.socialMediaVerified ? ArtistMobileViewMainTabs : MemberMobileViewMainTabs)
-    setMoreTabs( artistData?.socialMediaVerified ? ArtistMobileViewMoreTabs : MemberMobileViewMoreTabs)
-  }
-}
+  const getTabs = () => {
+    setMainTabs(artistData?.socialMediaVerified ? ArtistDesktopViewMainTabs : MemberDesktopViewMainTabs);
+    setMoreTabs(artistData?.socialMediaVerified ? ArtistDesktopViewMoreTabs : MemberDesktopViewMoreTabs);
+    if (isDeviceMobile) {
+      setMainTabs(artistData?.socialMediaVerified ? ArtistMobileViewMainTabs : MemberMobileViewMainTabs);
+      setMoreTabs(artistData?.socialMediaVerified ? ArtistMobileViewMoreTabs : MemberMobileViewMoreTabs);
+    }
+  };
   useEffect(() => {
     getTabs();
   }, [isDeviceMobile, artistData]);
 
-console.log("asdf  mainTabs::::",mainTabs);
-console.log("asdf  moreTabs::::",moreTabs)
+  return (
+    <Tabs>
+      <TabList theme={theme}>
+        {mainTabs.map((item) => (
+          <Tab index={item.key} theme={theme}>
+            {item.name}
+          </Tab>
+        ))}
+        <MoreTab baseIndex={mainTabs.length} theme={theme}>
+          {moreTabs.map((item) => (
+            <Tab index={item.key} theme={theme}>
+              {item.name}
+            </Tab>
+          ))}
+        </MoreTab>
+      </TabList>
 
-
-    return (
-          <Tabs>
-              <TabList>
-                  {mainTabs.map((item) => (
-                      <Tab index={item.key}>
-                          {item.name}
-                      </Tab>
-                  ))}
-                  <MoreTab baseIndex={mainTabs.length}>
-                      {moreTabs.map((item) => (
-                          <Tab index={item.key}>
-                              {item.name}
-                          </Tab>
-                      ))}
-                  </MoreTab>
-              </TabList>
-              {/*{mainTabs.concat(moreTabs).map((item, index) => (*/}
-              {/*    <TabPanel index={item.key}>*/}
-              {/*      {item.k}*/}
-              {/*        <p>Content of {item.name} Tab</p>*/}
-              {/*    </TabPanel>*/}
-
-              {/*))}*/}
-
-            {artistData.socialMediaVerified ? (
-                <>
-                  <TabPanel index={0}>
-                    {artistData.userId === currentUserId
-                        ? <DisplayLazyOwnedNfts />
-                        : <DisplayArtworks data={userArtworks} />}
-                  </TabPanel>
-                  <TabPanel index={1}>
-                    <DisplayCollections data={userCollections} />
-                  </TabPanel>
-                  <TabPanel index={2} style={{ margin: '25px 2%' }}>
-                    <div /> {/* Placeholder for any specific content if needed */}
-                  </TabPanel>
-                  <TabPanel index={3} style={{ margin: '25px 2%' }}>
-                    <h5 className="bioTabText">{artistData?.bio}</h5>
-                  </TabPanel>
-                </>
-            ) : (
-                <>
-                  <TabPanel index={0} style={{ margin: '25px 2%', width: 'auto' }}>
-                    {artistData.userId === currentUserId
-                        ? <DisplayMyOwnedNfts address={address} />
-                        : <DisplayMemberOwnedNfts address={artistData.userId} artistData={artistData} />}
-                  </TabPanel>
-                  {/* Assume other TabPanels for other tabs are defined here similarly */}
-                </>
-            )}
-          </Tabs>
+      {artistData.socialMediaVerified ? (
+        <>
+          <TabPanel index={0}>
+            {artistData.userId === currentUserId
+              ? <DisplayLazyOwnedNfts />
+              : <DisplayArtworks data={userArtworks} />}
+          </TabPanel>
+          <TabPanel index={1}>
+            <DisplayCollections data={userCollections} />
+          </TabPanel>
+          <TabPanel index={2}>
+            <DisplayDefaultTab />
+          </TabPanel>
+          <TabPanel index={3}>
+            <h5 className="bioTabText">{artistData?.bio}</h5>
+          </TabPanel>
+          <TabPanel index={4}>
+            <DisplayDefaultTab />
+          </TabPanel>
+          <TabPanel index={5}>
+            <DisplayDefaultTab />
+          </TabPanel>
+          <TabPanel index={6}>
+            <DisplayDefaultTab />
+          </TabPanel>
+        </>
+      ) : (
+        <>
+          <TabPanel index={0}>
+            {artistData.userId === currentUserId
+              ? <DisplayMyOwnedNfts address={address} />
+              : <DisplayMemberOwnedNfts address={artistData.userId} artistData={artistData} />}
+          </TabPanel>
+          <TabPanel index={1}>
+            <DisplayDefaultTab />
+          </TabPanel>
+          <TabPanel index={2}>
+            <DisplayDefaultTab />
+          </TabPanel>
+          <TabPanel index={3}>
+            <DisplayDefaultTab />
+          </TabPanel>
+          <TabPanel index={4}>
+            <DisplayDefaultTab />
+          </TabPanel>
+          <TabPanel index={5}>
+            <DisplayDefaultTab />
+          </TabPanel>
+        </>
+      )}
+    </Tabs>
   );
 }
 
--- src/components/layouts/tab/Tabs.jsx
+++ src/components/layouts/tab/Tabs.jsx
@@ -1,121 +1,109 @@
-/*eslint-disable*/
 import React, { useState, createContext, useContext } from 'react';
 import styled from 'styled-components';
-import {useMediaQuery} from "react-responsive";
-import Dropdown from "react-bootstrap/Dropdown";
-import {SlOptions} from "react-icons/sl";
+import Dropdown from 'react-bootstrap/Dropdown';
+import { SlOptions } from 'react-icons/sl';
+import { COLORS } from '../../shared/styles-constants';
 
-// Context to hold the active tab index
 const TabContext = createContext();
 
-// Styled components
 const TabListStyled = styled.div`
-  display: flex;
-  background: #fff;
-  border-bottom: 1px solid #e0e0e0;
-    border-top: 1px solid #e0e0e0;
+    display: flex;
+    background-color: ${(props) => (props.theme === 'light' ? 'white' : COLORS.BlackBG2)};
+    border-bottom: 1px solid transparent;
+    border-top: 1px solid transparent;
     padding-top: 15px;
     padding-bottom: 15px;
-
-    
-
+    border-color: ${(props) => (props.theme === 'light' ? '#e0e0e0' : 'transparent')};
 `;
 
 const TabStyled = styled.button`
-
     background-color: transparent;
-    // border: ${(props) => (props.theme === 'light' ? '2px solid black' : '2px solid white')};
     border: 2px solid transparent;
     padding: 0px 20px;
     height: 40px;
     font-size: initial;
-    color: black;
+    color: ${(props) => (props.theme === 'light' ? 'black' : 'white')};
     font-weight: bold;
     font-family: sans-serif;
     border-radius: 50px;
     margin: 0% 2%;
-    
-    
-  cursor: pointer;
-  //border: none;
-  //outline: none;
-  //border-bottom: 2px solid transparent;
-    
-    
+    cursor: pointer;
 
-  &:hover {
-    opacity: 1;
-      border: 2px solid black;
-  }
+    &:hover {
+        opacity: 1;
+        border-color: ${(props) => (props.theme === 'light' ? 'black' : 'white')};;
+    }
 
-  &.active {
-    opacity: 1;
-      background-color: green;
-      color: blue;
-      border: 2px solid transparent;
-  }
+    &.active {
+        opacity: 1;
+        background-color: ${(props) => (props.theme === 'light' ? 'black' : 'white')};
+        color: ${(props) => (props.theme === 'light' ? 'white' : 'black')};
+    }
 `;
 
 const TabPanelStyled = styled.div`
-  display: none;
+    display: none;
 
-  &.active {
-    display: block;
-      margin: 0% 2%;
-  }
+    &.active {
+        display: block;
+        margin: 0% 2%;
+    }
 `;
+export function Tabs({ children }) {
+  const [activeIndex, setActiveIndex] = useState(0);
+  return (
+    <TabContext.Provider value={{ activeIndex, setActiveIndex }}>
+      {children}
+    </TabContext.Provider>
+  );
+}
 
-// Components
-export const Tabs = ({ children }) => {
-    const [activeIndex, setActiveIndex] = useState(0);
-    return (
-        <TabContext.Provider value={{ activeIndex, setActiveIndex }}>
-            {children}
-        </TabContext.Provider>
-    );
-};
-
-export const TabList = ({ children }) => (
-    <TabListStyled>
-        {children}
+export function TabList({ children, theme }) {
+  return (
+    <TabListStyled theme={theme}>
+      {children}
     </TabListStyled>
-);
+  );
+}
 
-export const Tab = ({ children, index }) => {
-    const { activeIndex, setActiveIndex } = useContext(TabContext);
-    const onClick = () => setActiveIndex(index);
-    return(
-        <TabStyled className={activeIndex === index ? 'active' : ''} onClick={onClick}>
-            {children}
-        </TabStyled>
-    )
-};
+export function Tab({ children, index, theme }) {
+  const { activeIndex, setActiveIndex } = useContext(TabContext);
+  const onClick = () => setActiveIndex(index);
+  return (
+    <TabStyled
+      className={activeIndex === index ? 'active' : ''}
+      onClick={onClick}
+      theme={theme}
+    >
+      {children}
+    </TabStyled>
+  );
+}
 
-export const MoreTab = ({ children, baseIndex }) => {
-    const { setActiveIndex } = useContext(TabContext);
+export function MoreTab({ children, baseIndex, theme }) {
+  const { setActiveIndex } = useContext(TabContext);
 
-    return (
-        <Dropdown>
-            <Dropdown.Toggle id="profileTabDropdown">
-                <TabStyled><SlOptions /></TabStyled>
-            </Dropdown.Toggle>
-            <Dropdown.Menu>
-                {React.Children.map(children, (child, index) => (
-                    <Dropdown.Item onClick={() => setActiveIndex(baseIndex + index)}>
-                        {child.props.children}
-                    </Dropdown.Item>
-                ))}
-            </Dropdown.Menu>
-        </Dropdown>
-    );
-};
+  return (
+    <Dropdown>
+      <Dropdown.Toggle id="profileTabDropdown">
+        <TabStyled theme={theme}><SlOptions /></TabStyled>
+      </Dropdown.Toggle>
+      <Dropdown.Menu>
+        {React.Children.map(children, (child, index) => (
+          <Dropdown.Item onClick={() => setActiveIndex(baseIndex + index)}>
+            {child.props.children}
+          </Dropdown.Item>
+        ))}
+      </Dropdown.Menu>
+    </Dropdown>
+  );
+}
 
-export const TabPanel = ({ children, index }) => {
-    const { activeIndex } = useContext(TabContext);
-
-    return (
-        <TabPanelStyled className={activeIndex === index ? 'active' : ''}>
-            {children}
-        </TabPanelStyled>
-    );
-};
+export function TabPanel({ children, index }) {
+  const { activeIndex } = useContext(TabContext);
+  return (
+    <TabPanelStyled className={activeIndex === index ? 'active' : ''}>
+      {children}
+    </TabPanelStyled>
+  );
+}
