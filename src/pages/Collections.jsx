f7cebeb8edf6621f02d48ad00d2017e2ba1c6b67
fazal-a <thefazalabbas@gmail.com>
2024-06-24 23:06:39 +0500

    used collection card to render collections

--- src/pages/Collections.jsx
+++ src/pages/Collections.jsx
@@ -1,14 +1,14 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react';
 import { useSelector } from 'react-redux';
+import { useMediaQuery } from 'react-responsive';
 import Footer from '../components/footer/Footer';
+import { ArtistsContainer } from '../components/layouts/craftsmen/craftsmenStyles/CraftsMen.styles';
+import CollectionCard from '../components/layouts/Collections/CollectionCard';
 
 function Collections() {
   const collections = useSelector((state) => state.usersReducer.collections);
-  const [visible, setVisible] = useState(6);
-  const showMoreItems = () => {
-    setVisible((prevValue) => prevValue + 3);
-  };
+  const isDeviceMobile = useMediaQuery({ query: '(max-width: 767px)' });
+  const isDeviceTablet = useMediaQuery({ query: '(min-width: 768px) and (max-width: 1023px)' });
 
   const selectedTags = [];
 
@@ -26,142 +26,68 @@ function Collections() {
 
   return (
     <>
-      <div
-        className="margin-Left-Right-Top"
-      >
-        <div className="row">
-          <div className="col-md-12">
-            <h2 className=" ourArtists">
-              Collections
-            </h2>
-          </div>
-          <div className="col-12">
-            <div className="row tagsBar">
-              <div className="col-12">
-                <button
-                  type="button"
-                  className="tag"
-                  onClick={(e) => editTags(e.target.id, e.target)}
-                  onKeyDown={(e) => editTags(e.target.id, e.target)}
-                  id="painter"
-                >
-                  Painting
-                </button>
-                <button
-                  type="button"
-                  className="tag"
-                  onClick={(e) => editTags(e.target.id, e.target)}
-                  onKeyDown={(e) => editTags(e.target.id, e.target)}
-                  id="photographer"
-                >
-                  Photography
-                </button>
-                <button
-                  type="button"
-                  className="tag"
-                  onClick={(e) => editTags(e.target.id, e.target)}
-                  onKeyDown={(e) => editTags(e.target.id, e.target)}
-                  id="sculpturer"
-                >
-                  Sculpture
-                </button>
-                <button
-                  type="button"
-                  className="tag"
-                  onClick={(e) => editTags(e.target.id, e.target)}
-                  onKeyDown={(e) => editTags(e.target.id, e.target)}
-                  id="ceramic_artist"
-                >
-                  Mosaic
-                </button>
-                <button
-                  type="button"
-                  className="tag"
-                  onClick={(e) => editTags(e.target.id, e.target)}
-                  onKeyDown={(e) => editTags(e.target.id, e.target)}
-                  id="others"
-                >
-                  Others
-                </button>
-              </div>
-            </div>
-          </div>
-
-          {collections?.map((collection) => (
-            <div key={collection?.id} className="col-lg-4 col-md-6 col-12">
-              <div className="sc-card-collection style-2">
-                <div className="card-bottom">
-                  <div className="author">
-                    <div className="sc-author-box style-2">
-                      <div className="author-avatar">
-                        <img
-                          src={collection.owner_image}
-                          alt=""
-                          className="avatar"
-                        />
-                        <div className="badge" />
-                      </div>
-                    </div>
-                    <div className="content">
-                      <h4>
-                        <Link to={`/collection?id=${collection?.id}`}>
-                          {collection.name}
-                        </Link>
-                      </h4>
-                      <p>
-                        By
-                        {' '}
-                        <Link to={`/displayProfile?${collection?.owner_profile_type}=${collection?.owner}`}>
-                          <span className="authorName">{collection?.owner_name}</span>
-                        </Link>
-                      </p>
-                    </div>
-                  </div>
-                </div>
-
-                <Link to={`/collection?id=${collection?.id}`}>
-                  <div className="media-images-collection">
-                    <div className="box-left">
-                      <img
-                        src={collection.cover}
-                        alt=""
-                      />
-                    </div>
-                    <div className="box-right">
-                      <div className="top-img">
-                        <img
-                          src={collection.image}
-                          alt=""
-                        />
-                        <img
-                          src={collection.cover}
-                          alt=""
-                        />
-                      </div>
-                      <div className="bottom-img">
-                        <img
-                          src={collection.image}
-                          alt=""
-                        />
-                      </div>
-                    </div>
-                  </div>
-                </Link>
-              </div>
-            </div>
-          ))}
-          {visible < collections?.length && (
-          <div className="col-md-12 wrap-inner load-more text-center">
+      <div className="margin-Left-Right-Top">
+        <h2 className=" ourArtists">
+          Artists
+        </h2>
+        <div className="flex flex-column">
+          <div className="tagsBar flex">
             <button
               type="button"
-              id="load-more"
-              className="sc-button loadmore fl-button pri-3"
-              onClick={showMoreItems}
+              className="tag"
+              onClick={(e) => editTags(e.target.id, e.target)}
+              onKeyDown={(e) => editTags(e.target.id, e.target)}
+              id="painting"
             >
-              <span>Load More</span>
+              Painting
+            </button>
+            <button
+              type="button"
+              className="tag"
+              onClick={(e) => editTags(e.target.id, e.target)}
+              onKeyDown={(e) => editTags(e.target.id, e.target)}
+              id="photography"
+            >
+              Photography
+            </button>
+            <button
+              type="button"
+              className="tag"
+              onClick={(e) => editTags(e.target.id, e.target)}
+              onKeyDown={(e) => editTags(e.target.id, e.target)}
+              id="sculpture"
+            >
+              Sculpture
+            </button>
+            <button
+              type="button"
+              className="tag"
+              onClick={(e) => editTags(e.target.id, e.target)}
+              onKeyDown={(e) => editTags(e.target.id, e.target)}
+              id="mosaic"
+            >
+              Mosaic
+            </button>
+            <button
+              type="button"
+              className="tag"
+              onClick={(e) => editTags(e.target.id, e.target)}
+              onKeyDown={(e) => editTags(e.target.id, e.target)}
+              id="others"
+            >
+              Others
             </button>
           </div>
-          )}
+          <ArtistsContainer isDeviceMobile={isDeviceMobile} isDeviceTablet={isDeviceTablet}>
+            {collections?.map((collection) => (
+              <CollectionCard
+                key={collection.id}
+                collection={collection}
+                isDeviceMobile={isDeviceMobile}
+                isDeviceTablet={isDeviceTablet}
+              />
+            ))}
+          </ArtistsContainer>
         </div>
       </div>
       <Footer />
